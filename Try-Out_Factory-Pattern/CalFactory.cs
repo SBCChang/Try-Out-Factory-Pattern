35f4a390176b0077c2096be36a69576e6c319f17
SBC <iamsbc@gmail.com>
2018-12-10 23:16:02 +0800

    remove extra usings

--- Try-Out_Factory-Pattern/CalFactory.cs
+++ Try-Out_Factory-Pattern/CalFactory.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Try_Out_Factory_Pattern.Calculation;
 
 namespace Try_Out_Factory_Pattern
--- Try-Out_Factory-Pattern/Calculation/ICal.cs
+++ Try-Out_Factory-Pattern/Calculation/ICal.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Try_Out_Factory_Pattern.Calculation
+﻿namespace Try_Out_Factory_Pattern.Calculation
 {
     public interface ICal
     {
